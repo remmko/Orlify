ea4633f1d9a4e369f7fdfaf0e7428dc65b4a8118
remmko <stamovanton233@gmail.com>
2023-12-12 19:32:56 +0100

    7# WebApp configured, students ID in develop

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,10 +1,6 @@
-// Должно быть true в production
 var doCache = true;
-
-// Имя кэша
 var CACHE_NAME = 'my-pwa-cache-v2';
 
-// Очищает старый кэш
 self.addEventListener('activate', event => {
    const cacheWhitelist = [CACHE_NAME];
    event.waitUntil(
@@ -12,7 +8,6 @@ self.addEventListener('activate', event => {
            .then(keyList =>
                Promise.all(keyList.map(key => {
                    if (!cacheWhitelist.includes(key)) {
-                       console.log('Deleting cache: ' + key)
                        return caches.delete(key);
                    }
                }))
@@ -20,33 +15,27 @@ self.addEventListener('activate', event => {
    );
 });
 
-// 'install' вызывается, как только пользователь впервые открывает PWA 
 self.addEventListener('install', function(event) {
    if (doCache) {
        event.waitUntil(
            caches.open(CACHE_NAME)
                .then(function(cache) {
-                   // Получаем данные из манифеста (они кэшируются)
                    fetch('data/manifest.json')
                        .then(response => {
                            response.json()
                        })
                        .then(assets => {
-                       // Открываем и кэшируем нужные страницы и файлы
                            const urlsToCache = [
                                '/app/',
-             
                                '/static/core/logo.svg*',
                            ]
                            cache.addAll(urlsToCache)
-                           console.log('cached');
                        })
                })
        );
    }
 });
 
-// Когда приложение запущено, сервис-воркер перехватывает запросы и отвечает на них данными из кэша, если они есть
 self.addEventListener('fetch', function(event) {
    if (doCache) {
        event.respondWith(
