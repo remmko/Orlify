8cd6235a38a3352bacb8513339e7433e0965400c
aniolmorenobatlle <amoreno@cendrassos.net>
2023-12-19 17:10:22 +0100

    #32 search students

--- App/Views/index.php
+++ App/Views/index.php
@@ -4,7 +4,9 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
     <!-- Tailwind CSS -->
     <link rel="stylesheet" href="/main.css">
@@ -19,7 +21,7 @@
     <meta name="msapplication-navbutton-color" content="#7ACCE5">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="msapplication-starturl" content="/">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
     <link rel="manifest" href="data/manifest.json">
@@ -44,13 +46,13 @@
 </head>
 
 
-<body class="overflow-hidden">
+<body>
 
     <?php require_once("nav.php") ?>
 
 
 
-    <div class="mx-auto p-10 grid-container">
+    <div class="mx-auto mt-10 mr-10 ml-10 mb-5 grid-container">
         <!-- Side Navbar -->
         <div class="sidebar">
             <div class="left-menu text-xl">
@@ -87,19 +89,16 @@
         </div>
 
         <!-- Right Buttons -->
-        <div class="right-buttons">
+        <div class="right-buttons w-56">
+            <div class="max-w-md mx-auto">
+                <select id="alumnes-cerca"
+                    class="w-full border rounded-md px-4 py-2 focus:outline-none focus:border-blue-500 appearance-none bg-white">
+                    <!-- Opcions es generaran aquí -->
+                </select>
 
-            <form>
-                <div class="relative">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-2 pointer-events-none">
-                        <svg class="w-3 h-3 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
-                    </div>
-                    <input type="search" id="default-search" class="block w-full p-3 ps-7 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                </div>
+            </div>
 
-            </form>
+
 
             <div class="download">
                 <button>Descarregar</button>
@@ -109,7 +108,6 @@
     </div>
 
 
-    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="/js/flowbite.min.js"></script>
     <script src="/js/bundle.js"></script>
 </body>
